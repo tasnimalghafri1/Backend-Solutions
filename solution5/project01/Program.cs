02fd1765851d690e730aa736f9e030c4fed18d36
Tasnim AL-Ghafri <tasnimalghafri1@gmail.com>
2026-04-15 15:25:00 +0400

    Add files via upload
    Upload C# Practice Tasks
    Switch Case | for Loop | while Loop

---
+++ solution5/project01/Program.cs
@@ -0,0 +1,113 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1 – Day Name Printer:
+            //Console.WriteLine("Enter a number (1-7): ");
+            //int day = Convert.ToInt32(Console.ReadLine());
+
+            //switch (day)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Monday");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Tuesday");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Wednesday");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Thursday");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Friday");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Saturday");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Sunday");
+            //        break;
+            //    default:
+            //        if (!(day >= 1 && day <= 7))
+            //        {
+            //            Console.WriteLine("Invalid day number");
+            //        }
+            //        break;
+            //}
+
+
+            //Task 2 – Multiplication Table
+            //Console.WriteLine("Enter a number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(number + " x " + i + " = " + (number * i));
+            //}
+
+            //Task 3 – Countdown Timer:
+            //Console.Write("Enter a positive number: ");
+            //int number = int.Parse(Console.ReadLine());
+
+            //if (number <= 0)
+            //{
+            //    Console.WriteLine("Please enter a positive number");
+            //}
+            //else
+            //{
+            //    while (number >= 1)
+            //    {
+            //        Console.WriteLine(number);
+            //        number = number-1;
+            //    }
+            //    Console.WriteLine("Go!");
+
+            //}
+
+            //Task 4 – Season Detector with Month Validation:
+            Console.WriteLine("Enter a month number (1-12): ");
+            int month = int.Parse(Console.ReadLine());
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Winter");
+                    break;
+
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Spring");
+                    break;
+
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Summer");
+                    break;
+
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Autumn");
+                    break;
+
+                default:
+                    if (month < 1 || month > 12)
+                    {
+                        Console.WriteLine("Invalid month number");
+                    }
+                    break;
+            }
+        }
+    }
+}
