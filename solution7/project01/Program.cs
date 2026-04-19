91e0006008325b09fa4799e9a02ce61f5f5814b6
Tasnim AL-Ghafri <tasnimalghafri1@gmail.com>
2026-04-19 14:42:45 +0400

    Add files via upload
    Types of error

---
+++ solution7/project01/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Error Types:
+
+            //1.syntax error:
+
+            //Console.WriteLine("enter num 1:");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter num 2:");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(num1/num1);
+
+            //string name = null;
+            //Console.WriteLine(name.Length);
+
+            //2.runtime error:
+
+            //Console.WriteLine("enter num 1:");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter num 2:");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(num1 / num1);
+            //Console.WriteLine("enter num 1:");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter num 2:");
+            //int y = Convert.ToInt32(Console.ReadLine());
+
+            //try
+            //{
+            //    Console.WriteLine(x/y);
+            //}
+            //catch (Exception tasnim)
+            //{
+            //        Console.WriteLine(tasnim.Message);
+            //  }
+            //finally
+            //{
+            //    int result = Convert.ToInt32(Console.ReadLine());
+            //    int final = result / y;
+            //    Console.WriteLine(final);
+            //}
+
+            //int say(int x, int y)
+            //{
+            //    return x + y;
+            //}
+            #endregion
+            //say(45);
+
+
+
+        }
+    }
+}
