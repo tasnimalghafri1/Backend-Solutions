9c685f422311c1b66feec3604f7c5b32c75ca1aa
Tasnim AL-Ghafri <tasnimalghafri1@gmail.com>
2026-04-20 11:08:34 +0400

    Add files via upload
    loop

---
+++ solution8/project01/Program.cs
@@ -0,0 +1,177 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //task1 :
+
+            //int[] num = { 10, 20, 30, 40, 50 };
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    Console.WriteLine(num[i]);
+
+            //}
+
+
+            //task2:
+
+            //int[] num = { 10, 20, 30, 40, 50 };
+            //foreach (int i in num)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+            //task3:
+            //int[] num = new int[5];
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    Console.WriteLine("Enter any number:");
+            //    num[i] = Convert.ToInt32(Console.ReadLine());
+
+            //}
+            //Console.WriteLine("/*/*/*/*/*/*/*/*/*");
+
+            //foreach (int i in num)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //task4:
+            //int[] numbers = new int[5];
+            //int sum = 0;
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(" pls Enter number: ");
+            //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+
+            //    sum += numbers[i]; 
+            //}
+
+
+            //Console.WriteLine("Sum = " + sum);
+
+            //task5:
+            //int[] num = new int[5];
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(" pls Enter number: ");
+            //    num[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //int max = num[0];
+
+            //for (int i = 1;i<5;i++)
+            //{
+            //    if (num[i] > max)
+            //    {
+            //        max = num[i];
+            //    }
+            //}
+            //Console.WriteLine("Max = " +max);
+
+            //task6:
+            //int[] num = new int[5];
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(" pls Enter number: ");
+            //    num[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //int min = num[0];
+
+            //for (int i = 1; i< 5; i++)
+            //{
+            //    if (num[i] < min)
+            //    {
+            //        min = num[i];
+            //    }
+            //}
+            //Console.WriteLine("Min = " + min);
+
+            //task7:
+            //int[] num = new int[10];
+
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Please enter number: ");
+            //    num[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (num[i] % 2 == 0)
+            //    {
+            //        Console.WriteLine("Even number = " + num[i]);
+            //    }
+            //}
+
+
+            //task8:
+            //int[] num = new int[5];
+            //int sum = 0;
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(" pls Enter number: ");
+            //    num[i] = Convert.ToInt32(Console.ReadLine());
+            //    sum += num[i];
+            //}
+            //double avg = (double)sum / 5;
+            //Console.WriteLine("Average number =" + avg);
+
+
+            //task9:
+            //int[] num = new int[5];
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("Please enter number: ");
+            //    num[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine("Reversed numbers:");
+            //for (int i = 4; i >= 0; i--)
+            //{
+            //    Console.WriteLine(num[i]);
+            //}
+
+            //task10:
+
+            //int[] numbers = new int[5];
+
+           
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.Write("Enter number: ");
+            //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+          
+            //Console.Write("Enter number to search: ");
+            //int target = Convert.ToInt32(Console.ReadLine());
+
+            //int index = -1; 
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    if (numbers[i] == target)
+            //    {
+            //        index = i;
+            //        break;
+            //    }
+            //}
+
+          
+            //if (index != -1)
+            //    Console.WriteLine("Number found at index " + index);
+            //else
+            //    Console.WriteLine("Number not found");
+
+        }
+    }
+}
