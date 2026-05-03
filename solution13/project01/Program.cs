bebdddcea188bde052d4973839204711b5dba672
Tasnim AL-Ghafri <tasnimalghafri1@gmail.com>
2026-05-03 13:44:00 +0400

    Add files via upload

---
+++ solution13/project01/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace project01
+{
+    internal class Program
+    {
+        //static void Increase( out int x) {
+
+        //    x = 1;
+        //}
+        //int num;//1
+
+        //int num ;//1
+        //Increase( out num);
+        //Console.WriteLine(num);//1
+        // ref : initial value - yes //change value : may//Usage:change exist value
+        //out : initial value - no //change value : must//Usage:Assign new value
+
+        // Recursion : A method that calls itself to solve a problem.
+
+        static void Hello()
+        {
+            Console.WriteLine("Plz enter your age");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 18)
+                Hello();
+            else
+                return;
+        }
+
+        static void Main(string[] args)
+        {
+            Hello();
+        }
+
+    }
+}
+
+
+   
+
+
